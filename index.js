ee69d1faab07b0fe09ff80ec0d557698578b0b00
alvarofblanco <alvarofblanco@gmail.com>
2022-03-25 08:30:19 -0300

    Profile added

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+require('dotenv').config();
 const debug = require('debug')('app');
+const morgan = require('morgan');
 const express = require('express');
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const { getCard } = require('./src/cardService');
 
+app.use(morgan('dev'));
+
 app.get('/', async (req, res) => {
   const { cardName } = req.query;
-  debug('Card Name', cardName);
+  if (cardName === undefined) {
+    return res.status(400).send('no cardName provided');
+  }
 
   let arr;
 
   try {
     arr = await getCard(cardName);
+    if (arr.length === 0) {
+      throw new Error('No card founded');
+    }
   } catch (e) {
     return res.status(500).json({ message: 'Sorry, an error ocurred', error: e.message });
   }
@@ -19,4 +30,6 @@ app.get('/', async (req, res) => {
   return res.json({ message: `${cardName} OK`, prices: arr });
 });
 
-app.listen(3000, () => debug('App listening on port 3000'));
+app.use((req, res) => res.status(404).send('Sorry cant find that'));
+
+app.listen(PORT, () => debug(`App listening on port ${PORT}`));
